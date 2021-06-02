056ea2cd51810a430407bcf22fd491d646db39f8
ivan.gnatyuk <ivan.gnatyuk@sitesoft.ru>
2021-06-02 12:54:57 +0500

    Moved console staff to another file

---
+++ GIConsoleExample/AppDelegate+DebugConsole.swift
@@ -0,0 +1,78 @@
+//
+//  AppDelegate+DebugConsole.swift
+//  GIConsoleExample
+//
+//  Created by ivan.gnatyuk on 02.06.2021.
+//
+
+import UIKit
+import GIAppDebugConsole
+
+extension AppDelegate {
+    
+    func setupDebugConsoleIfNeeded() {
+        configConsole()
+    }
+    
+    func showDebugConsoleIfNeeded() {
+        GIAppDebugConsole.shared.show()
+    }
+    
+}
+
+
+// MARK: - DebugConsole config
+private extension AppDelegate {
+    
+    func configConsole() {
+        // You don't have to configure anything. Only if you want.
+        ///*
+        GIAppDebugConsoleConfigurator.configureAppDebugConsole(
+            consoleUIConfig: .init(consoleDefaultSize: .init(width: 300, height: 200),
+                                   consoleMaxSize: .init(width: 500, height: 500),
+                                   consoleBackgroundColor: .red,
+                                   textColor: .white,
+                                   font: .systemFont(ofSize: 15, weight: .bold),
+                                   menuButtonConfig: .init(size: .init(width: 34, height: 34),
+                                                           backgroundColor: .black,
+                                                           image: .init(named: "ellipsis"),
+                                                           title: nil,
+                                                           tintColor: .green,
+                                                           alpha: 0.7)))
+         //*/
+        
+        
+        // Add custom actions to console menu
+        
+        GIAppDebugConsole.shared.addAction(.init(title: "Copy Push token", handler: { [weak self] in
+            self?.copyPushToken()
+        }))
+        
+        GIAppDebugConsole.shared.addAction(.init(title: "Do a barrel roll", handler: { [weak self] in
+            self?.doBarrelRoll()
+        }))
+    }
+    
+}
+
+
+// MARK: - DebugConsole actions
+private extension AppDelegate {
+    
+    func doBarrelRoll() {
+        let viewToRoll = UIApplication.shared.windows.first
+        UIView.animate(withDuration: 1) {
+            viewToRoll?.transform = .init(rotationAngle: .pi)
+        } completion: { _ in
+            viewToRoll?.transform = .identity
+        }
+    }
+    
+    func copyPushToken() {
+        let pushToken = "YOUR_PUSH_TOKEN"
+        
+        UIPasteboard.general.string = pushToken
+        print("push token copied")
+    }
+    
+}
--- GIConsoleExample/AppDelegate.swift
+++ GIConsoleExample/AppDelegate.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import GIAppDebugConsole
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,62 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        configConsole()
+        setupDebugConsoleIfNeeded()
         return true
     }
     
     func applicationDidBecomeActive(_ application: UIApplication) {
-        GIAppDebugConsole.shared.show()
-    }
-    
-}
-
-
-// MARK: - DebugConsole config
-private extension AppDelegate {
-    
-    func configConsole() {
-        GIAppDebugConsoleConfigurator.configureAppDebugConsole(
-            consoleUIConfig: .init(consoleDefaultSize: .init(width: 300, height: 200),
-                                   consoleMaxSize: .init(width: 500, height: 500),
-                                   consoleBackgroundColor: .red,
-                                   textColor: .white,
-                                   font: .systemFont(ofSize: 20, weight: .bold),
-                                   menuButtonConfig: .init(size: .init(width: 50, height: 50),
-                                                           backgroundColor: .black,
-                                                           image: .init(named: "ellipsis"),
-                                                           title: nil,
-                                                           tintColor: .blue, alpha: 0.7)))
-        
-        GIAppDebugConsole.shared.addAction(.init(title: "Copy Push token", handler: { [weak self] in
-            self?.copyPushToken()
-        }))
-        
-        GIAppDebugConsole.shared.addAction(.init(title: "Do a barrel roll", handler: { [weak self] in
-            self?.doBarrelRoll()
-        }))
-    }
-    
-}
-
-
-// MARK: - DebugConsole actions
-private extension AppDelegate {
-    
-    func doBarrelRoll() {
-        let viewToRoll = UIApplication.shared.windows.first
-        UIView.animate(withDuration: 1) {
-            viewToRoll?.transform = .init(rotationAngle: .pi)
-        } completion: { _ in
-            viewToRoll?.transform = .identity
-        }
-    }
-    
-    func copyPushToken() {
-        let pushToken = "YOUR_PUSH_TOKEN"
-        
-        UIPasteboard.general.string = pushToken
-        print("push token copied")
+       showDebugConsoleIfNeeded()
     }
     
 }
